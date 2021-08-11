948402c01a59a4ae8a3f9add2153b8e749cc0a9b
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-08-11 22:38:15 +0200

    adding textbox class + state + behaviour

---
+++ realsoftwareclass/textbox.java
@@ -0,0 +1,19 @@
+package realsoftwareclass;
+
+public class textbox {
+    
+    // state
+
+    int text;
+    int limit;
+    int length;
+    boolean isFocused;
+
+    // behaviour
+
+    public void setText(){}
+    public void clear(){}
+    public void enable(){}
+    public void disable(){}
+
+}
