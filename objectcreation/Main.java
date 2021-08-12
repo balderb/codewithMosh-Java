983e7c7f68c275b0e522d9170079c7dc0fdf306c
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-08-12 09:14:08 +0200

    populating objectcreation/Main.java with content

--- objectcreation/Main.java
+++ objectcreation/Main.java
@@ -1,11 +1,14 @@
 package objectcreation;
 
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 public class Main {
     public static void main (String[] args){
         
         // let's create a new "instance" of the textbox class like this:
         var textBox1 = new TextBox();
         textBox1.setText("Box 1");
+        System.out.print(textBox1.text);
     }
 }
 
