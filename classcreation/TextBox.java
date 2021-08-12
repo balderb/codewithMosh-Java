306a7763995fc3dc0712ff485523c8d7c29e8ad1
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-08-12 08:37:17 +0200

    adding some additional notes to the textbox class + renaming the package of this course from creating-classes to classcreation

---
+++ classcreation/TextBox.java
@@ -0,0 +1,17 @@
+package creating-classes;
+
+
+// this "public" keyword we have here is an "access modifier" as we call it.
+// It defines who can actually get access to this particular class
+// 
+public class TextBox {
+
+    // let's add three members: (1) textfield, (2) , (3)
+    
+
+}
+
+
+
+
+
