75c8634799f46e6031970fe12c5266659e38a36a
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-08-12 08:45:58 +0200

    adding additional notes to classcreation package alongside the course

--- classcreation/TextBox.java
+++ classcreation/TextBox.java
@@ -1,6 +1,5 @@
 package creating-classes;
 
-
 // this "public" keyword we have here is an "access modifier" as we call it.
 // It defines who can actually get access to this particular class
 // 
@@ -8,7 +7,9 @@ public class TextBox {
 
     // let's add three members: (1) textfield, (2) , (3)
     
-    public str text; // field
+    // pay attention: return types should be with capital letters
+
+    public String text; // field
 
     // now let's define methods
     // with void we do not return any values from this method
@@ -19,11 +20,9 @@ public class TextBox {
 
     // clear the text of the textbox
 
-    public void clear()
+    public void clear(){};
 
 }
 
-
-
-
+// technically, we don't use fields with the access modifier as "public" -> but we make it private and get access to the fields using getters and (possibly) setters
 
