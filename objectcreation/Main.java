02314434c8641f54fdd59fbee03a04ee43141c39
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-08-12 09:05:23 +0200

    populating objectcreation classes with content on making new instances etc..

--- objectcreation/Main.java
+++ objectcreation/Main.java
@@ -3,7 +3,10 @@ package objectcreation;
 public class Main {
     public static void main (String[] args){
         
+        // let's create a new "instance" of the textbox class like this:
+        TextBox textBox1 = new TextBox();
     }
 }
 
+// in java variables have a static type which means that, once we set the type of a variable, we cannot change it anymore.
 
