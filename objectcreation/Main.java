f5354afe598e0baf2f0f56a8698e51fe5c39303d
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-08-13 18:05:01 +0200

    startup course today

--- objectcreation/Main.java
+++ objectcreation/Main.java
@@ -1,12 +1,24 @@
 package objectcreation;
 
+import realsoftwareclass.textbox;
+
 public class Main {
     public static void main (String[] args){
         
+        
+        
+
+
         // let's create a new "instance" of the textbox class like this:
         var textBox1 = new TextBox();
         textBox1.setText("Box 1");
         System.out.print(textBox1.text);
+
+        // let's also create another textbox object
+
+        var textBox2 = new textbox();
+        textBox2.setText("Box 2");
+
     }
 }
 
